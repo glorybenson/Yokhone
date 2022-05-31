793cf4965f046dc77425c6bd40fd87d71e0a2e3e
Ore A <oreadeleye67@gmail.com>
2022-05-31 08:24:29 +0100

    fr lang update

--- resources/views/layouts/includes/nav.blade.php
+++ resources/views/layouts/includes/nav.blade.php
@@ -43,7 +43,7 @@
             </a>
             <div class="dropdown-menu">
                 <a class="dropdown-item" href="{{ route('my.profile') }}"><i class="feather-user"></i>
-                    {{ __('Cancel') }}
+                    {{ App::getLocale() }}
                 </a>
                 <a class="dropdown-item" href="{{ route('change.password') }}"><i class="fa fa-lock"></i> Change Password</a>
                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="feather-power"></i> Logout
