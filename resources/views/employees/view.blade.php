500e6ca9c919ab6795322f046f5775c9026ff89f
Glory Benson <glorybenson3738@gmail.com>
2022-07-04 22:39:30 +0100

    Update view.blade.php

--- resources/views/employees/view.blade.php
+++ resources/views/employees/view.blade.php
@@ -29,13 +29,13 @@
                             <div class="row">
                                 <div class="col-xm-2">
                                     <div class="text-center">
-                                        <a href="#" class="btn btn-primary p-2" style="border-radius: 18px 18px 0px 0px;">{{$employee->first_name}} {{$employee->last_name }}</a>
+                                        <a href="#" class="btn btn-primary p-2" style="border-radius: 18px 18px 0px 0px; margin-top: 5px;">{{$employee->first_name}} {{$employee->last_name }}</a>
                                     </div>
                                 </div>
 
                                 <div class="col-xm-2">
                                     <div class="text-center">
-                                        <a href="{{ route('absence.employee', $employee->id) }}" class="btn btn-light active" style="border-radius: 18px 18px 0px 0px;">
+                                        <a href="{{ route('absence.employee', $employee->id) }}" class="btn btn-light active" style="border-radius: 18px 18px 0px 0px; margin-top: 5px">
                                             {{__('Absence')}}
                                         </a>
                                     </div>
@@ -43,7 +43,7 @@
 
                                 <div class="col-xm-2">
                                     <div class="text-center">
-                                        <a href="{{ route('record.employee', $employee->id) }}" class="btn btn-light active" style="border-radius: 18px 18px 0px 0px;">
+                                        <a href="{{ route('record.employee', $employee->id) }}" class="btn btn-light active" style="border-radius: 18px 18px 0px 0px; margin-top: 5px">
                                             {{__('Employee Record')}}
                                         </a>
                                     </div>
@@ -51,7 +51,7 @@
 
                                 <div class="col-xm-2">
                                     <div class="text-center">
-                                        <a href="{{ route('salary.employee', $employee->id) }}" class="btn btn-light active" style="border-radius: 18px 18px 0px 0px;">
+                                        <a href="{{ route('salary.employee', $employee->id) }}" class="btn btn-light active" style="border-radius: 18px 18px 0px 0px; margin-top: 5px">
                                             {{__('Salary History')}}
                                         </a>
                                     </div>
@@ -59,7 +59,7 @@
                                 <div class="col-xm-2">
                                     <div class="text-center">
                                         <a href="{{ route('payment.employee', $employee->id) }}" class="btn btn-light
-                                         active" style="border-radius: 18px 18px 0px 0px;">{{__('Payment')}}</a>
+                                         active" style="border-radius: 18px 18px 0px 0px; margin-top: 5px">{{__('Payment')}}</a>
                                     </div>
                                 </div>
                                 <div class="card-body">
